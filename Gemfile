8270beab3a830b63d280b30aca2e51df596f1a6b
Kenia Guimaraes <keniiarruda@gmail.com>
2018-01-04 22:57:27 +0000

    Adicionando funcionalidade de busca pelo  wikipedia

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 gem 'http'
 gem 'listen', '~> 3.0.5'
+gem "wikipedia-client"
 
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   post '/' => 'callback#recieved_data'
   get 'callback/recieved_data'
   resources :callback
+  resources :assistant
   root 'callback#index'
   root 'messages#index'
+  	
+  post '/messenger' => 'assistant#messenger'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
